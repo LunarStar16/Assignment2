b4815e32f9bf0d695636fe46fd21f4999de1e2a1
Lexa <liam.wright@uleth.ca>
2022-12-02 17:46:14 -0700

    Added back camera functionality

--- www/js/index.js
+++ www/js/index.js
@@ -1,34 +1,34 @@
 
-// document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('deviceready', onDeviceReady, false);
 
-// function onDeviceReady() {
-//     // Cordova is now initialized. Have fun!
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
 
-//     var options = {
-//         quality: 100,
-//         destinationType: Camera.DestinationType.FILE_URI
-//     }
+    var options = {
+        quality: 100,
+        destinationType: Camera.DestinationType.FILE_URI
+    }
 
-//     $("#takePhoto").on("click", takePic);
+    $("#takePhoto").on("click", takePic);
 
-//     function takePic() {
-//         navigator.camera.getPicture(onSuccess, onError, options)
-//     }
+    function takePic() {
+        navigator.camera.getPicture(onSuccess, onError, options)
+    }
 
-//     function onSuccess(imageData) {
-//         console.log(imageData);
-//         resolveLocalFileSystemURL(imageData, function (fileEntry) {
-//             var myNewImage = fileEntry.toURL()
-//             console.log(myNewImage);
-//             // do something with URL, assign to src or create an html
-//             $("#takePhoto").after("<div class='picture'><img src='" + myNewImage + "'></div>") 
-//         }, onError);
-//     }
-//     function onError(message){
-//         alert("Photo Not Taken Because" + message)
-//     }
+    function onSuccess(imageData) {
+        console.log(imageData);
+        resolveLocalFileSystemURL(imageData, function (fileEntry) {
+            var myNewImage = fileEntry.toURL()
+            console.log(myNewImage);
+            // do something with URL, assign to src or create an html
+            $("#takePhoto").after("<div class='picture'><img src='" + myNewImage + "'></div>") 
+        }, onError);
+    }
+    function onError(message){
+        alert("Photo Not Taken Because" + message)
+    }
 
-// }
+}
 
 
 //code from: https://youtu.be/KfLqRuFjK5g / https://openprocessing.org/sketch/952881 / https://editor.p5js.org/Andrew_Sink/sketches/YM-Ply_cD
